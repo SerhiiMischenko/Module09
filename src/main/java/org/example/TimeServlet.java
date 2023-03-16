1a8c0fb6e0ec7d777a95fb4948530f3f1eaa04f1
SerhiiMischenko <serj.roshen@gmail.com>
2023-03-16 13:59:14 +0200

    Update TimeServlet.java to query parameter

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -13,8 +13,18 @@ import java.time.format.DateTimeFormatter;
 public class TimeServlet extends HttpServlet {
         @Override
         protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            ZonedDateTime now;
+            String timeZone = req.getParameter ("timezone");
+            if(timeZone == null) {
+                now = ZonedDateTime.now(ZoneId.of("UTC"));
+                resp.setContentType("text/html; charset=utf-8");
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+                String formattedDate = now.format(formatter);
+                resp.getWriter().write(formattedDate);
+                resp.getWriter().close();
+            }
+            now = ZonedDateTime.now(ZoneId.of(timeZone.replace(" ", "+")));
             resp.setContentType("text/html; charset=utf-8");
-            ZonedDateTime now = ZonedDateTime.now(ZoneId.of("UTC"));
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
             String formattedDate = now.format(formatter);
             resp.getWriter().write(formattedDate);
