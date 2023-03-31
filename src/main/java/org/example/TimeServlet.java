1848102f631beecb2f41877064a22f02351593b9
SerhiiMischenko <serj.roshen@gmail.com>
2023-03-31 11:00:44 +0300

    First commit

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,13 +23,28 @@ public class TimeServlet extends HttpServlet {
                 String formattedDate = now.format(formatter);
                 resp.getWriter().write(formattedDate);
                 resp.getWriter().close();
+            }else {
+                Cookie cookie = new Cookie("lastTimezone", timeZone.replace(" ", "+"));
+                cookie.setMaxAge(24 * 60 * 60);
+                resp.addCookie(cookie);
+                Cookie[] cookies = req.getCookies();
+                for (int i = 0; i < cookies.length; i++) {
+                    now = ZonedDateTime.now(ZoneId.of(cookies[0].getValue()));
+                    resp.setContentType("text/html; charset=utf-8");
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+                    String formattedDate = now.format(formatter);
+                    resp.getWriter().write(formattedDate);
+                    resp.getWriter().close();
+                }
+//                for (Cookie coo : cookies) {
+//                    now = ZonedDateTime.now(ZoneId.of(coo.getValue()));
+//                    resp.setContentType("text/html; charset=utf-8");
+//                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+//                    String formattedDate = now.format(formatter);
+//                    resp.getWriter().write(formattedDate);
+//                    resp.getWriter().close();
+//                }
             }
-            now = ZonedDateTime.now(ZoneId.of(timeZone.replace(" ", "+")));
-            resp.setContentType("text/html; charset=utf-8");
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
-            String formattedDate = now.format(formatter);
-            resp.getWriter().write(formattedDate);
-            resp.getWriter().close();
         }
     }
 
